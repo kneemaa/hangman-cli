0022f0f49fe6c95945d129200c30285d9109ea67
Nema Darban <nema.darban@gmail.com>
2018-02-08 14:20:28 -0800

    word contructor partially built

--- word.js
+++ word.js
@@ -1,3 +1,20 @@
 var Letter = require("./letter.js");
 
 
+var Word = function(word) {
+	this.wordArray = [];
+	this.assembleLetters = function(){
+		for (i = 0; i < word.length; i++){
+			var characters = word[i];
+			var uniqueLetter = new Letter(characters);
+			this.wordArray.push(uniqueLetter);
+		}
+	};
+};
+
+
+var nema = new Word("nema");
+
+nema.assembleLetters();
+
+console.log(nema.wordArray);
